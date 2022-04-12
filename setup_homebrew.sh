32d5574a06ae8b9d0d938f1e5d8ae0db2a33bc99
Belal Samy <38237387+belalsamyyy@users.noreply.github.com>
2022-04-12 21:31:08 +0200

    Install Alfred
    Using --no-quarantine ( for all casks ) to disable gatekeeper

--- setup_homebrew.sh
+++ setup_homebrew.sh
@@ -5,10 +5,11 @@ echo "\n <<< Starting Homebrew setup >>> \n"
 # install homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-# brews ( Packages)
+# brews ( Packages )  
 brew install httpie 
 brew install bat 
 
 # casks ( Applications )
-brew install --cask google-chrome 
-brew install --cask visual-studio-code
+brew install --cask --no-quarantine google-chrome 
+brew install --cask --no-quarantine visual-studio-code
+brew install --cask --no-quarantine alfred
