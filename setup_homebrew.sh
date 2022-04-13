dba2d6f302f2355c85fd13513589fa8fab387400
Belal Samy <38237387+belalsamyyy@users.noreply.github.com>
2022-04-13 23:36:41 +0200

    Use exists() to prevent unnecessary Homebrew installs

--- setup_homebrew.sh
+++ setup_homebrew.sh
@@ -3,7 +3,13 @@
 echo "\n <<< Starting Homebrew setup >>> \n"
 
 # install homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+if exists brew; then 
+  echo "Homebrew already installed 👍, skip install ..."
+else 
+  echo "Installing Homebrew ..."
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi 
 
 # install taps, brews & casks from Brewfile
 brew bundle --verbose 
